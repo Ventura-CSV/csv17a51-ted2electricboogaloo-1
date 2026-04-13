f6a64f4433697ba9fc1939386b03ce2a2130ac81
ted2electricboogaloo <michaeleris598@gmail.com>
2026-04-13 16:59:10 -0700

    describe what you changed

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def get_range(mapping: dict) -> set:
     """Return the range — the set of outputs actually mapped to."""
     # === TODO ===
     # Your code here
+    return set( mapping.values() )
     pass
     # === END TODO ===
 
