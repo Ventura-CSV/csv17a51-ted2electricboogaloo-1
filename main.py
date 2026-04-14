d77d5fdd7a684baa01aae253a00d1a9caab0a158
ted2electricboogaloo <michaeleris598@gmail.com>
2026-04-13 17:02:28 -0700

    describe what you changed

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def is_well_defined(mapping: dict, target: set) -> bool:
     """Return True if every output value is in the target set."""
     # === TODO ===
     # Your code here
+    return set( mapping.values() ).issubset( target )
     pass
     # === END TODO ===
 
